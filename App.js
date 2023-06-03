0a49bcf52745795bffadecee0f850448dec46a3f
manoj-patle <manojpatle2011@gmail.com>
2023-06-04 00:56:44 +0530

    done-2

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 const dataClass = document.querySelector('.data');
 const mainBodyData = document.querySelector('.mainBody');
 const filterData = document.querySelector('.filterData');
+const btn = document.querySelectorAll('.btn');
 const timeFrames = {
   city: 'Place',
   state: 'State',
   center: 'name',
 };
-let timeStamp = timeFrames['city'];
-function setTimeFrame(time) {
-  timeStamp = timeFrames[time];
-  // console.log(timeStamp);
+let timeStamp = '';
+function setTimeFrame(selected) {
+  btn.forEach((bt) => (bt.style.background = ''));
+  if (selected == 'city') {
+    btn[0].style.background = 'blue';
+  } else if (selected == 'state') {
+    btn[1].style.background = 'blue';
+  }
+  if (selected == 'center') {
+    btn[2].style.background = 'blue';
+  }
+  timeStamp = timeFrames[selected];
+  console.log(timeStamp);
 }
 async function getData() {
   const resp = await fetch('https://isro.vercel.app/api/centres');
@@ -48,17 +58,18 @@ async function searchInput() {
   data.centres.forEach((element) => {
     delete element['id'];
   });
+
+  // const filtData = data.centres.filter((center) => center.`${timeStamp}`.toLowerCase() === inputData.toLowerCase());
+  // console.log(filtData);
+
   if (timeStamp === 'Place') {
     const filtData = data.centres.filter((center) => center.Place.toLowerCase() === inputData.toLowerCase());
-    console.log(filtData);
     filteredData(filtData);
   } else if (timeStamp === 'State') {
     const filtData = data.centres.filter((center) => center.State.toLowerCase() === inputData.toLowerCase());
-    console.log(filtData);
     filteredData(filtData);
   } else if (timeStamp === 'name') {
     const filtData = data.centres.filter((center) => center.name.toLowerCase() === inputData.toLowerCase());
-    console.log(filtData);
     filteredData(filtData);
   }
 }
