7a4ba9d486bf74ce54719bbb7f7c531040b9f3ad
manoj-patle <manojpatle2011@gmail.com>
2023-06-03 20:15:11 +0530

    3rd June Update

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 const dataClass = document.querySelector('.data');
-// console.log(dataClass);
-const getData = async () => {
+const mainBodyData = document.querySelector('.mainBody');
+const filterData = document.querySelector('.filterData');
+const timeFrames = {
+  city: 'Place',
+  state: 'State',
+  center: 'name',
+};
+let timeStamp = timeFrames['city'];
+function setTimeFrame(time) {
+  timeStamp = timeFrames[time];
+  // console.log(timeStamp);
+}
+async function getData() {
   const resp = await fetch('https://isro.vercel.app/api/centres');
   const data = await resp.json();
-  //   console.log(data);
-  //   console.log(data.centres);
   data.centres.forEach((element) => {
     delete element['id'];
   });
-  //   console.log(data.centres);
   appendData(data.centres);
-};
+}
 getData();
-
 appendData = (data) => {
   data.forEach((element) => {
     const obj = Object.keys(element);
@@ -21,13 +28,37 @@ appendData = (data) => {
       const td = document.createElement('td');
       const divtHead = document.createElement('div');
       const divData = document.createElement('div');
-      divtHead.className = 'tab__tbody--div';
       divtHead.textContent = key === 'name' ? 'Center' : key === 'Place' ? 'City' : 'State';
       divData.textContent = element[key];
       td.append(divtHead, divData);
       tr.append(td);
-      document.querySelector('tbody').append(tr);
+      mainBodyData.append(tr);
     });
-    // document.querySelector('.hi').append(tr);
   });
 };
+
+async function searchInput() {
+  const inputData = document
+    .getElementById('inp')
+    .value.toLowerCase()
+    .replace(/\b\w/g, (s) => s.toUpperCase());
+  mainBodyData.style.display = 'none';
+  const resp = await fetch('https://isro.vercel.app/api/centres');
+  const data = await resp.json();
+  const newData = data.centres.forEach((element) => {
+    delete element['id'];
+    element.filter((obj) => {
+      return timeStamp[inputData];
+    });
+  });
+  console.log(newData);
+  // const new Data = data.centres.filter(obj)
+  // console.log(data.centres);
+  // filteredData(data.centres);
+}
+
+// const filteredData = (data) => {
+//   const filteredArray = data.filter((obj) => console.log(obj););
+//   // console.log(filteredArray);
+//   // filteredArray = arr.filter( obj => obj.(selected) === "(input)" );
+// };
